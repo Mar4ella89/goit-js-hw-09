a4320ded5a3b73fff0cb00d2fe49f42727b1c3e9
Marat Baikin <3371335@gmail.com>
2022-11-28 17:54:44 +0200

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,36 +1,96 @@
-// import flatpickr from "flatpickr";
-// import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-const ref = {
-  btnStart: document.querySelector("button[data-start]"),
-  outputeValue: document.querySelector('.timer')
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  outputeValue: document.querySelector('.timer'),
+  outputeDays: document.querySelector('span[data-days]'),
+  outputeHours: document.querySelector('span[data-hours]'),
+  outputeMinutes: document.querySelector('span[data-minutes]'),
+  outputeSeconds: document.querySelector('span[data-seconds]'),
 };
 
-console.log(ref.btnStart);
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] < new Date()) {
+      window.alert('Error');
+      return;
+    }
+    refs.btnStart.removeAttribute('disabled');
+  },
+};
 
-const onBtnClickStartTimer = () => {};
+refs.btnStart.setAttribute('disabled', true);
 
-ref.btnStart.addEventListener("click", onBtnClickStartTimer);
+const selectTime = flatpickr('#datetime-picker', options);
+
+const addLeadingZero = value => {
+  return String(value).padStart(2, '0');
+};
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-  console.log(convertMs(2000)); 
-  console.log(convertMs(140000));
-  console.log(convertMs(24140000));
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+const updateClockFace = ({ days, hours, minutes, seconds }) => {
+  refs.outputeDays.textContent = `${days}`;
+  refs.outputeHours.textContent = `${hours}`;
+  refs.outputeMinutes.textContent = `${minutes}`;
+  refs.outputeSeconds.textContent = `${seconds}`;
+};
+
+const timer = {
+  start() {
+    const timerId = setInterval(() => {
+      refs.btnStart.setAttribute('disabled', true);
+
+      const currentTime = Date.now();
+      console.log(currentTime);
+
+      const startTime = selectTime.latestSelectedDateObj.getTime();
+      console.log(startTime);
+
+      if (startTime < currentTime) {
+        return;
+      }
+
+      const deltaTime = startTime - currentTime;
+      const timeComponents = convertMs(deltaTime);
+      console.log(timeComponents);
+      updateClockFace(timeComponents);
+    }, 1000);
+  },
+};
+
+const onBtnClickStartTimer = () => {
+  timer.start();
+  clearInterval(timerId);
+};
+
+refs.btnStart.addEventListener('click', onBtnClickStartTimer);
+
+Notiflix.Notify.success('Click Me', {
+  timeout: 6000,
+});
