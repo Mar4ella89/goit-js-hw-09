f6dbadfb7edda82b9ca3d7eaa17208c44aecc276
Marat Baikin <3371335@gmail.com>
2022-11-29 03:50:19 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,58 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  inputeForm: document.querySelector('.form'),
+  createPromisesBtn: document.querySelector('.form button'),
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(
+          { position, delay }
+        );
+      } else {
+        reject(
+          { position, delay }
+        );
+      }
+    }, delay);
+  });
 }
+
+const onSubmitCreatePromise = event => {
+  event.preventDefault();
+   const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  
+  const sumDelay = Number(delay.value) + Number(step.value)
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, sumDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          {
+            clickToClose: true,
+            timeout: 3000,
+          }
+        )
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          {
+            clickToClose: true,
+            timeout: 3000,
+          }
+        )
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+};
+
+refs.inputeForm.addEventListener('submit', onSubmitCreatePromise);
